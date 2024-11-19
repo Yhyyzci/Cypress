4379f3452b32c66a98e687dc612aa871b9d055fc
Yhyyzci <k95599k@gmail.com>
2024-11-19 22:50:25 +0300

    19.11

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,8 +1,17 @@
 describe('Login Testi', () => {
+
+
     it('Giriş yapabilmeli', () => {
         // Ana sayfayı ziyaret et
         cy.visit('https://www.kidokit.com/');
+        // İstediğiniz testlerin geri kalanını buraya ekleyin
+        // Örneğin, giriş yapıldıktan sonra kullanıcıyı doğrulamak:
+        cy.url().should('include', '/dashboard'); // Örnek doğrulama
+
+        // XPath ile butona tıkla
+        cy.xpath("//button[contains(@style, 'line')]").click({ force: true });
 
 
-    })
-})
+    });
+
+});
